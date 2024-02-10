d1f08e80a7a52be61718b150df9d0670c05331fd
sharronq <sharronsq@gmail.com>
2024-02-10 15:58:43 -0500

    fixed main tester code

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-from taipy import Gui
-from taipy import Config
-from taipy import Core
+# IMPORT STATEMENTS
+from taipy import Gui, Config, Core
 import taipy as tp
 
+# FUNCTION DEFINITIONS
 def build_message(name: str):
     return f"Hello, {name}!"
-
-def submit_scenario(state):
-    state.scenario.input_name.write(state.input_name)
-    state.scenario.submit(wait=True)
-    state.message = scenario.message.read()
-
 
 input_name_data_node_cfg = Config.configure_data_node(id="input_name")
 message_data_node_cfg = Config.configure_data_node(id="message")
 build_message_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
 scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_message_task_cfg])
 
+input_name = "Taipy"
+message = None
+
+def submit_scenario(state):
+    state.scenario.input_name.write(state.input_name)
+    state.scenario.submit()
+    state.message = scenario.message.read()
+
 page = """
 Name: <|{input_name}|input|>
+
 <|submit|button|on_action=submit_scenario|>
+
 Message: <|{message}|text|>
 """
 
+print("Hello world1")
 if __name__ == "__main__":
     Core().run()
     scenario = tp.create_scenario(scenario_cfg)
     Gui(page).run()
 
+print("Hello world2")
 
 # hello_scenario = tp.create_scenario(scenario_cfg)
 # hello_scenario.input_name.write("Taipy")
