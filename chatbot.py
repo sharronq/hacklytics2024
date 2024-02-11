a725f2c433e4997269e47116d98b2a217cc408ea
sharronq <sharronsq@gmail.com>
2024-02-10 22:02:08 -0500

    10:00pm chatbot attempt

--- chatbot.py
+++ chatbot.py
@@ -6,8 +6,9 @@ import json
 import predictionguard as pg
 from getpass import getpass
 
+user_text = ""
 # pg_access_token = getpass("q1VuOjnffJ3NO2oFN8Q9m8vghYc84ld13jaqdF7E")
-os.environ['PREDICTIONGUARD_TOKEN'] = ACCESS_TOKEN
+os.environ['PREDICTIONGUARD_TOKEN'] = "q1VuOjnffJ3NO2oFN8Q9m8vghYc84ld13jaqdF7E"
 pg.Chat.list_models()
 messages = [
     {
@@ -46,34 +47,33 @@ messages = [
     }
 ]
 print('Welcome to the Chatbot! Let me know how can I help you')
-
-while True:
-  print('')
-  request = input('User' + ': ')
-  if request=="Stop" or request=='stop':
-      print('Bot: Bye!')
-      break
-  else:
-      messages.append({
-          "role": "user",
-          "content": request
-      })
-      response = pg.Chat.create(
-          model="Neural-Chat-7B",
-          messages=messages
-      )['choices'][0]['message']['content'].split('\n')[0].strip()
-      messages.append({
-          "role": "assistant",
-          "content": response
-      })
-      print('Bot: ', response)
+request = input('User' + ': ') # Terminal input
+# while True:
+#   print('')
+#   request = input('User' + ': ')
+#   if request=="Stop" or request=='stop':
+#       print('Bot: Bye!')
+#       break
+#   else:
+#       messages.append({
+#           "role": "user",
+#           "content": request
+#       })
+#       response = pg.Chat.create(
+#           model="Neural-Chat-7B",
+#           messages=messages
+#       )['choices'][0]['message']['content'].split('\n')[0].strip()
+#       messages.append({
+#           "role": "assistant",
+#           "content": response
+#       })
+#       print('Bot: ', response)
 
 # context = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today? "
-# conversation = {
-#     "Conversation": ["Who are you?", "Hi! I am GPT-3. How can I help you today?"]
-# }
+conversation = {
+    "Conversation": ["Hello!", "Welcome to the Chatbot! Let me know how can I help you"] }
 
-# current_user_message = ""
+current_user_message = ""
 
 # client = openai.Client(api_key="q1VuOjnffJ3NO2oFN8Q9m8vghYc84ld13jaqdF7E")
 
@@ -99,30 +99,34 @@ while True:
 #     )
 #     return response.choices[0].message.content
 
-# def send_message(state: State) -> None:
+def send_message(state: State) -> None:
+    if request=="Stop" or request=='stop':
+      print('Bot: Bye!')
+    
 #     """
 #     Send the user's message to the API and update the conversation.
 
 #     Args:
 #         - state: The current state.
 #     """
-#     # Add the user's message to the context
-#     state.context += f"Human: \n {state.current_user_message}\n\n AI:"
-#     # Send the user's message to the API and get the response
-#     answer = request(state, state.context).replace("\n", "")
-#     # Add the response to the context for future messages
-#     state.context += answer
-#     # Update the conversation
-#     conv = state.conversation._dict.copy()
-#     conv["Conversation"] += [state.current_user_message, answer]
-#     state.conversation = conv
-#     # Clear the input field
-#     state.current_user_message = ""
+    messages.append({
+          "role": "user",
+          "content": request
+      })
+    response = pg.Chat.create(
+          model="Neural-Chat-7B",
+          messages=messages
+      )['choices'][0]['message']['content'].split('\n')[0].strip()
+    messages.append({
+          "role": "assistant",
+          "content": response
+      })
+    print('Bot: ', response)
 
-# page = """
-# <|{conversation}|table|show_all|width=100%|>
-# <|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|>
-# """
+page = """
+<|{conversation}|table|show_all|width=100%|>
+<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|>
+"""
 
 
-# Gui(page).run(title="Taipy Chat", use_reloader=True, port=5001)
+Gui(page).run(title="Taipy Chat", use_reloader=True, port=5001)
